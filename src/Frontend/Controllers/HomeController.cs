4f37e8098de6e84be7b13092c43bdfded09b9603
Anna Kublitskaya <kublitskayavd@yandex.ru>
2018-06-10 20:01:49 +0300

    lab 1

---
+++ src/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+using System.Net;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static string m_URL = "http://localhost:5003/api/values";
+        private HttpClient m_httpClient = new HttpClient();
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(string data)
+        {
+            if (data == null)
+            {
+                return Ok("");
+            }
+            else
+            {
+                List<KeyValuePair<string, string>> values = new List<KeyValuePair<string, string>>(){
+                    new KeyValuePair<string, string>("value", data),
+                };
+                var content = new FormUrlEncodedContent(values);
+                var request = m_httpClient.PostAsync(m_URL, content);
+                var requestContent = request.Result.Content.ReadAsStringAsync();
+                return Ok(requestContent.Result);
+            }
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
